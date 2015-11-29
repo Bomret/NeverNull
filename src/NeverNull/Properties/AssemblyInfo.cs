d40f9f7ba58dd3adc54a4666e1f6404c629a2b99
Stefan Reichel <dev@bomret.com>
2015-11-29 12:26:26 +0100

    Bumped up release notes.

--- src/NeverNull/Properties/AssemblyInfo.cs
+++ src/NeverNull/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("NeverNull")]
 [assembly: AssemblyProductAttribute("NeverNull")]
 [assembly: AssemblyDescriptionAttribute("Project has no summmary; update build.fsx")]
-[assembly: AssemblyVersionAttribute("1.0")]
-[assembly: AssemblyFileVersionAttribute("1.0")]
+[assembly: AssemblyVersionAttribute("3.0")]
+[assembly: AssemblyFileVersionAttribute("3.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0";
+        internal const string Version = "3.0";
     }
 }
